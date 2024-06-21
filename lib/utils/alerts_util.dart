dbacc70bd41dbd7b55b532ec96d531ca485f1611
YahyaAhmedKhan <yahyaahmed.52@gmail.com>
2024-06-21 16:07:48 +0500

    added platform specific alert box

--- lib/utils/alerts_util.dart
+++ lib/utils/alerts_util.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
 
 class Alerts {
   static void showAlertDialog(BuildContext context, String responseText) {
-    customAlertWidget(context, responseText, () {});
+    if (Platform.isIOS) {
+      customAlertWidget(context, responseText, () {});
+    } else if (Platform.isAndroid) {
+      customAlertDialogAndroid(context, responseText);
+    }
   }
 
   static void customAlertTokenWidget(
@@ -11,41 +16,81 @@ class Alerts {
     String responseText,
     VoidCallback onClose,
   ) {
-    showCupertinoDialog(
-      context: context,
-      builder: (BuildContext context) => CupertinoAlertDialog(
-        title: Text(
-          responseText,
-        ),
-        actions: [
-          CupertinoDialogAction(
-            child: const Text(
-              "Close",
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-              onClose(); // Call the callback to handle the action
-            },
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+          title: Text(
+            responseText,
           ),
-        ],
-      ),
-    );
+          actions: [
+            CupertinoDialogAction(
+              child: const Text(
+                "Close",
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+                onClose(); // Call the callback to handle the action
+              },
+            ),
+          ],
+        ),
+      );
+    } else if (Platform.isAndroid) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: Text(responseText),
+          actions: [
+            TextButton(
+              child: const Text("Close"),
+              onPressed: () {
+                Navigator.pop(context);
+                onClose(); // Call the callback to handle the action
+              },
+            ),
+          ],
+        ),
+      );
+    }
   }
 
-  // Original customAlertWidget
   static void customAlertWidget(
-      BuildContext context, String responseText, Null Function() param2) {
-    showCupertinoDialog(
-      context: context,
-      builder: (BuildContext context) => CupertinoAlertDialog(
-        title: Text(
-          responseText,
-        ),
-        actions: [
-          CupertinoDialogAction(
-            child: const Text(
-              "OK",
+      BuildContext context, String responseText, VoidCallback onClose) {
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+          title: Text(
+            responseText,
+          ),
+          actions: [
+            CupertinoDialogAction(
+              child: const Text(
+                "OK",
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+                onClose(); // Call the callback to handle the action
+              },
             ),
+          ],
+        ),
+      );
+    } else if (Platform.isAndroid) {
+      customAlertDialogAndroid(context, responseText);
+    }
+  }
+
+  static void customAlertDialogAndroid(
+      BuildContext context, String responseText) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: Text(responseText),
+        actions: [
+          TextButton(
+            child: const Text("OK"),
             onPressed: () {
               Navigator.pop(context);
             },
@@ -56,27 +101,43 @@ class Alerts {
   }
 
   static void showLoadingDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      barrierDismissible: false, // Prevents dismissal by tapping outside
-      builder: (BuildContext context) {
-        return Dialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+        context: context,
+        builder: (BuildContext context) => const CupertinoAlertDialog(
+          title: Text("Loading..."),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: 16),
+              CircularProgressIndicator(),
+            ],
           ),
-          child: const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                CircularProgressIndicator(),
-                SizedBox(width: 16),
-                Text('Loading...'),
-              ],
+        ),
+      );
+    } else if (Platform.isAndroid) {
+      showDialog(
+        context: context,
+        barrierDismissible: false, // Prevents dismissal by tapping outside
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
             ),
-          ),
-        );
-      },
-    );
+            child: const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(width: 16),
+                  Text('Loading...'),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+    }
   }
 }
