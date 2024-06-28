b8d9b13b2aec2148925e9567dcef5ab1c1fb5964
YahyaAhmedKhan <yahyaahmed.52@gmail.com>
2024-06-28 23:20:55 +0500

    added handleimag

--- lib/utils/button_utils.dart
+++ lib/utils/button_utils.dart
@@ -1,5 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:safify/Action%20Team%20Module/providers/all_action_reports_provider.dart';
+import 'package:safify/Admin%20Module/providers/delete_action_report_provider.dart';
+import 'package:safify/models/action_report.dart';
 import 'package:safify/models/report.dart';
 
 void handleImageButton(String? imageUrl, BuildContext context) {
@@ -53,4 +57,50 @@ void handleImageButton(String? imageUrl, BuildContext context) {
       },
     );
   }
+}
+
+void showRejectActionReportDialogue(BuildContext context, ActionReport item) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text("Reject?"),
+        content: Text("Are you sure you want to reject this report?"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(); // Close the dialog
+            },
+            child: Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: () {
+              DeleteActionReportProvider deleteActionReportProvider =
+                  Provider.of<DeleteActionReportProvider>(context,
+                      listen: false);
+
+              deleteActionReportProvider
+                  .deleteActionReport('${item.action_report_id}')
+                  .then((success) async {
+                if (success) {
+                  Navigator.of(context).pop(); // Close the dialog
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    backgroundColor: Colors.greenAccent,
+                    content: Text('Report deleted'),
+                    duration: Duration(seconds: 2),
+                  ));
+                  await Provider.of<ActionReportsProvider>(context,
+                          listen: false)
+                      .fetchAllActionReports(context);
+
+                  //PUSH NOTIFICATION
+                }
+              });
+            },
+            child: Text("Confirm"),
+          ),
+        ],
+      );
+    },
+  );
 }
